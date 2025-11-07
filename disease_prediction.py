06acd1c23fde6fbb573ae84b28d290bd488a3744
Deepak Avachitkar <avachitkardeepak@gmail.com>
2025-11-07 22:01:33 +0530

    Update disease_prediction.py

--- disease_prediction.py
+++ disease_prediction.py
@@ -2,7 +2,6 @@ import os
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
-import google.generativeai as genai
 import requests
 import json
 
@@ -187,6 +186,7 @@ if selected == "AI Health Assistant 🤖":
         st.success(st.session_state.reply)
 
             
+
 
 
 
